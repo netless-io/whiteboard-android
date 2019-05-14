1e2b424410fe9c583fdf8e632b7578552ce104a2
leavesster <a412739861@qq.com>
2019-05-14 12:06:53 +0800

    add startActivity

---
+++ app/src/main/java/com/herewhite/demo/StartActivity.java
@@ -0,0 +1,51 @@
+package com.herewhite.demo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class StartActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.whiteSDKDemo.UUID";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public String getUuid() {
+        EditText text = findViewById(R.id.editText);
+        String uuid = text.getText().toString();
+        return uuid;
+    }
+
+    public void createRoom(View view) {
+        Intent intent = new Intent(this, RoomActivity.class);
+        startActivity(intent);
+    }
+
+    public void joinRoom(View view) {
+        Intent intent = new Intent(this, RoomActivity.class);
+        if (getUuid().length() > 0) {
+            intent.putExtra(EXTRA_MESSAGE, getUuid());
+        }
+        startActivity(intent);
+    }
+
+    public void replayRoom(View view) {
+        Intent intent = new Intent(this, RoomActivity.class);
+        if (getUuid().length() > 0) {
+            intent.putExtra(EXTRA_MESSAGE, getUuid());
+            startActivity(intent);
+        } else {
+            Log.w("error", "none uuid");
+        }
+    }
+}
