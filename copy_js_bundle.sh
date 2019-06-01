e47be85b6748635f1f404e7da2d8f14e81af8eb9
leavesster <a412739861@qq.com>
2019-06-01 17:16:36 +0800

    update script

--- copy_js_bundle.sh
+++ copy_js_bundle.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
+BASEDIR=$(cd $(dirname "$0"); pwd -P)
+set -exo pipefail
+cd $BASEDIR
+
 rm -rf sdk/src/main/assets/cloud/
 mkdir sdk/src/main/assets/cloud
-cp -R ../white-sdk/native-sdk-render/build/index.html sdk/src/main/assets/cloud/
-cp -R ../white-sdk/native-sdk-render/build/static sdk/src/main/assets/cloud/
+cp -R ../white-native-sdk/build/index.html sdk/src/main/assets/cloud/
+cp -R ../white-native-sdk/build/static sdk/src/main/assets/cloud/
