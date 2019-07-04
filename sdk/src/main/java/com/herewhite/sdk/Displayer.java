808a2be40033031f364854df99bef9cb8ff114c5
leavesster <11785335+leavesster@users.noreply.github.com>
2019-07-04 19:39:41 +0800

    add image API

--- sdk/src/main/java/com/herewhite/sdk/Displayer.java
+++ sdk/src/main/java/com/herewhite/sdk/Displayer.java
@@ -1,8 +1,15 @@
 package com.herewhite.sdk;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 
 import com.google.gson.Gson;
+import com.herewhite.sdk.domain.Promise;
+import com.herewhite.sdk.domain.SDKError;
+
+import wendu.dsbridge.OnReturnValue;
 
 public class Displayer {
 
@@ -17,6 +24,48 @@ public class Displayer {
         this.bridge = bridge;
         this.context = context;
         this.sdk = sdk;
+    }
+
+    public void getScenePreviewImage(String scenePath, final Promise<Bitmap>promise) {
+        this.bridge.callHandler("displayerAsync.scenePreview", new Object[]{scenePath}, new OnReturnValue<String>() {
+            @Override
+            public void onValue(String retValue) {
+                Bitmap bitmap = null;
+                try {
+                    bitmap = transformBase64toBitmap(retValue);
+                } catch (Exception e) {
+                    promise.catchEx(new SDKError(e.getMessage()));
+                }
+                if (bitmap != null) {
+                    promise.then(bitmap);
+                }
+            }
+        });
+    }
+
+    public void getSceneSnapshotImage(String scenePath, final Promise<Bitmap>promise) {
+        this.bridge.callHandler("displayerAsync.sceneSnapshot", new Object[]{scenePath}, new OnReturnValue<String>() {
+            @Override
+            public void onValue(String retValue) {
+                Bitmap bitmap = null;
+                try {
+                    bitmap = transformBase64toBitmap(retValue);
+                } catch (Exception e) {
+                    promise.catchEx(new SDKError(e.getMessage()));
+                }
+                if (bitmap != null) {
+                    promise.then(bitmap);
+                }
+            }
+        });
+    }
+
+    private Bitmap transformBase64toBitmap(String base64String) {
+        final String pureBase64Encoded = base64String.substring(base64String.indexOf(",")  + 1);
+        final byte[] decodedBytes = Base64.decode(pureBase64Encoded, Base64.DEFAULT);
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        opts.inDensity = context.getResources().getDisplayMetrics().densityDpi;
+        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length, opts);
     }
 
     public void moveCamera(Object camera) {
