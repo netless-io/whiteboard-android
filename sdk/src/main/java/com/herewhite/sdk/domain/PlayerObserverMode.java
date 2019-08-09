2365949ed710ceefd7e60c95b6ea42217fc83793
leavesster <11785335+leavesster@users.noreply.github.com>
2019-08-09 19:25:05 +0800

    PlayerObserverMode javadoc

--- sdk/src/main/java/com/herewhite/sdk/domain/PlayerObserverMode.java
+++ sdk/src/main/java/com/herewhite/sdk/domain/PlayerObserverMode.java
@@ -1,6 +1,17 @@
 package com.herewhite.sdk.domain;
 
+/**
+ * The enum Player observer mode.
+ */
 public enum  PlayerObserverMode {
+
+    /**
+     * 跟随模式，默认跟随主播，或者第一个用户
+     */
     directory, //跟随模式
+
+    /**
+     * 自由模式
+     */
     freedom    //自由模式
 }
