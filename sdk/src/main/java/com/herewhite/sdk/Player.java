00d979242515edf30c6d9a67a2a40e4e0016c62c
leavesster <11785335+leavesster@users.noreply.github.com>
2019-08-09 19:24:21 +0800

    player javadoc comment

--- sdk/src/main/java/com/herewhite/sdk/Player.java
+++ sdk/src/main/java/com/herewhite/sdk/Player.java
@@ -16,6 +16,9 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import wendu.dsbridge.OnReturnValue;
 
+/**
+ * 回放房间操作类
+ */
 public class Player extends Displayer {
 
     private final ConcurrentHashMap<String, EventListener> eventListenerConcurrentHashMap = new ConcurrentHashMap<>();
@@ -29,6 +32,16 @@ public class Player extends Displayer {
 
     private PlayerPhase playerPhase = PlayerPhase.waitingFirstFrame;
 
+    /**
+     * Instantiates a new Player.
+     *
+     * @param room            回放房间 uuid
+     * @param bridge          the bridge
+     * @param context         the context
+     * @param whiteSdk        the white sdk
+     * @param playerTimeInfo  the player time info
+     * @param syncPlayerState the sync player state
+     */
     public Player(String room, WhiteBroadView bridge, Context context, WhiteSdk whiteSdk, PlayerTimeInfo playerTimeInfo, SyncDisplayerState<PlayerState> syncPlayerState) {
         super(room, bridge, context, whiteSdk);
         this.syncPlayerState = syncPlayerState;
@@ -57,15 +70,21 @@ public class Player extends Displayer {
         bridge.callHandler("player.pause", new Object[]{});
     }
 
-    //stop 后，player 资源会被释放。需要重新创建WhitePlayer实例，才可以重新播放
+    /**
+     * stop 后，player 资源会被释放。需要重新创建WhitePlayer实例，才可以重新播放
+     */
     public void stop() {
         bridge.callHandler("player.stop", new Object[]{});
         this.sdk.releasePlayer(uuid);
     }
 
-    //跳转至特定时间，开始时间为 0，单位毫秒
-    public void seekToScheduleTime(long beginTime) {
-        bridge.callHandler("player.seekToScheduleTime", new Object[]{beginTime});
+    /**
+     * 跳转至特定时间，开始时间为 0，单位毫秒。
+     *
+     * @param seekTime 跳转时间
+     */
+    public void seekToScheduleTime(long seekTime) {
+        bridge.callHandler("player.seekToScheduleTime", new Object[]{seekTime});
     }
 
     //region Event API
@@ -80,18 +99,34 @@ public class Player extends Displayer {
         }
     }
 
+    /**
+     * 移除特定自定义事件监听
+     *
+     * @param eventName 自定义事件名称
+     */
     public void removeMagixEventListener(String eventName) {
         this.eventListenerConcurrentHashMap.remove(eventName);
         bridge.callHandler("player.removeMagixEventListener", new Object[]{eventName});
     }
 
+    /**
+     * 添加自定义事件监听
+     *
+     * @param eventName 自定义事件名称
+     * @param eventListener 自定义事件回调实现 @see EventListener
+     */
     public void addMagixEventListener(String eventName, EventListener eventListener) {
         this.eventListenerConcurrentHashMap.put(eventName, eventListener);
         bridge.callHandler("player.addMagixEventListener", new Object[]{eventName});
     }
     //endregion
 
-    //设置查看模式
+    /**
+     * 设置查看模式
+     *
+     * @param mode 查看模式
+     * @see PlayerObserverMode
+     */
     public void setObserverMode(PlayerObserverMode mode) {
         bridge.callHandler("player.setObserverMode", new Object[]{mode.name()});
     }
@@ -99,13 +134,21 @@ public class Player extends Displayer {
     //region Get API
 
     /**
-     * 获取房间状态
+     * 同步 获取回放房间播放状态
      * 目前：初始状态为 WhitePlayerPhaseWaitingFirstFrame
+     *
+     * @return 回放房间状态
      */
     public PlayerPhase getPlayerPhase() {
         return this.playerPhase;
     }
 
+    /**
+     * 异步 获取回放房间播放状态
+     *
+     * @deprecated 使用 {@link #getPlayerPhase()} 同步 API，进行获取。
+     * @param promise the promise
+     */
     @Deprecated
     public void getPhase(final Promise<PlayerPhase> promise) {
         bridge.callHandler("player.getBroadcastState", new Object[]{}, new OnReturnValue<Object>() {
@@ -127,13 +170,23 @@ public class Player extends Displayer {
     }
 
     /**
+     * 同步 获取回放房间中所有状态
+     *
      * 当 phase 状态为 WhitePlayerPhaseWaitingFirstFrame
      * 回调得到的数据是空的
+     *
+     * @return 回放房间状态 {@link PlayerState}，与 {@link com.herewhite.sdk.domain.RoomState 有接近的结构}
      */
     public PlayerState getPlayerState() {
         return this.syncPlayerState.getDisplayerState();
     }
 
+    /**
+     * 异步 获取回放房间中状态
+     *
+     * @deprecated 请使用 {@link #getPlayerState()} 同步 API，进行获取。
+     * @param promise 完成回调
+     */
     @Deprecated
     public void getPlayerState(final Promise<PlayerState> promise) {
         bridge.callHandler("player.state.playerState", new Object[]{}, new OnReturnValue<Object>() {
@@ -155,11 +208,21 @@ public class Player extends Displayer {
         });
     }
 
-    /** 获取播放器信息（当前时长，总时长，开始 UTC 时间戳）单位：毫秒 */
+    /**
+     * 同步 获取播放文件信息
+     * 包括：当前时间，总时长，开始 UTC 时间戳。单位：毫秒
+     * @return {@link PlayerTimeInfo}
+     */
     public PlayerTimeInfo getPlayerTimeInfo() {
         return new PlayerTimeInfo(this.scheduleTime, this.timeDuration, this.framesCount, this.beginTimestamp);
     }
 
+    /**
+     * 异步 获取播放文件信息
+     *
+     * @deprecated 请使用 {@link #getPlayerTimeInfo()} 同步 API，进行获取。
+     * @param promise 完成回调
+     */
     @Deprecated
     public void getPlayerTimeInfo(final Promise<PlayerTimeInfo> promise) {
         bridge.callHandler("player.state.timeInfo", new Object[]{}, new OnReturnValue<Object>() {
