57c39655873c67d723ca09a02f269d161d2f62ea
leavesster <11785335+leavesster@users.noreply.github.com>
2019-08-10 10:28:18 +0800

    update roomMember field

--- sdk/src/main/java/com/herewhite/sdk/domain/RoomMember.java
+++ sdk/src/main/java/com/herewhite/sdk/domain/RoomMember.java
@@ -6,30 +6,56 @@ package com.herewhite.sdk.domain;
 
 public class RoomMember {
     private Long memberId;
-    private String currentApplianceName;
     private MemberInformation information;
 
+    /**
+     * 用户的所有教具信息
+     *
+     * @return 教具信息
+     * @see MemberState
+     * @since 2.4.7
+     */
+    public MemberState getMemberState() {
+        return memberState;
+    }
+
+    private MemberState memberState;
+
+    /**
+     * 在加入房间时，带入的用户信息，可以为任意内容,建议各端传入字典。
+     *
+     * @return 用户 payload
+     * @since 2.4.7
+     */
+    public Object getPayload() {
+        return payload;
+    }
+
+    private Object payload;
+
+    /**
+     * 在白板内部对应的用户自增 id。
+     *
+     * @return 内部用户 id
+     */
     public long getMemberId() {
         return memberId;
     }
 
-    public void setMemberId(long memberId) {
-        this.memberId = memberId;
-    }
-
+    /**
+     * 返回当前用户的教具类型
+     *
+     * @deprecated 请使用 {@link #getMemberState()} 获取教具详细信息
+     * @return 教具名称
+     * @since 2.4.7
+     */
+    @Deprecated
     public String getCurrentApplianceName() {
-        return currentApplianceName;
-    }
-
-    public void setCurrentApplianceName(String currentApplianceName) {
-        this.currentApplianceName = currentApplianceName;
+        return memberState.getCurrentApplianceName();
     }
 
     public MemberInformation getInformation() {
         return information;
     }
 
-    public void setInformation(MemberInformation information) {
-        this.information = information;
-    }
 }
