216e6a1b490df5f1f2bb2f6dddcf8924487d3fde
leavesster <11785335+leavesster@users.noreply.github.com>
2019-08-10 12:59:20 +0800

    roomParams java doc comment

--- sdk/src/main/java/com/herewhite/sdk/RoomParams.java
+++ sdk/src/main/java/com/herewhite/sdk/RoomParams.java
@@ -3,8 +3,6 @@ package com.herewhite.sdk;
 import com.herewhite.sdk.domain.MemberInformation;
 import com.herewhite.sdk.domain.WhiteObject;
 
-import org.json.JSONObject;
-
 /**
  * Created by buhe on 2018/8/11.
  */
@@ -19,6 +17,15 @@ public class RoomParams extends WhiteObject {
         return userPayload;
     }
 
+    /**
+     * 配置需要透传的用户信息，推荐使用 {@link com.google.gson.JsonObject} key-value object
+     *
+     * 如果需要显示用户头像地址，请在用户信息的 avatar 字段中，添加用户头像图片地址。
+     * 从 {@link MemberInformation} 迁移，只需要在 userPayload 中，传入相同字段即可。
+     *
+     * @param userPayload 用户信息，完全自由定义，会被完整传递
+     * @since 2.0.0
+     */
     public void setUserPayload(Object userPayload) {
         this.userPayload = userPayload;
     }
@@ -29,10 +36,15 @@ public class RoomParams extends WhiteObject {
         this(uuid, roomToken, (Object) null);
     }
 
-    /*
-    * memberInfo 已弃用，请使用功能更强的 userPayload
-    * @deprecated
-    * */
+    /**
+     * 配置实时房间参数
+     *
+     * @see MemberInformation 已弃用，现已支持更高自由度的用户信息定义
+     * @param uuid       实时房间 uuid
+     * @param roomToken  实时房间 token
+     * @param memberInfo 用户信息，仅限对应字段
+     * @deprecated 请使用 {@link #RoomParams(String, String, Object)} 传入用户信息。
+     */
     @Deprecated
     public RoomParams(String uuid, String roomToken, MemberInformation memberInfo) {
         this.uuid = uuid;
@@ -40,18 +52,35 @@ public class RoomParams extends WhiteObject {
         this.memberInfo = memberInfo;
     }
 
-    /*
-    * userPayload 需要为String，Number，Null 等可以
-    * */
+    /**
+     * 配置实时房间参数
+     *
+     * @param uuid       实时房间 uuid
+     * @param roomToken  实时房间 token
+     * @param userPayload 自定义用户字段，参考 {@link #setUserPayload(Object)} 说明
+     * @since 2.0.0
+     */
     public RoomParams(String uuid, String roomToken, Object userPayload) {
         this.uuid = uuid;
         this.roomToken = roomToken;
         this.userPayload = userPayload;
     }
 
+    /**
+     * 获取设置的用户信息
+     *
+     * @return 用户信息
+     * @deprecated 请使用 {@link #getUserPayload()}
+     */
     @Deprecated
     public MemberInformation getMemberInfo() { return memberInfo; }
 
+    /**
+     * 设置用户信息
+     *
+     * @param memberInfo {@link MemberInformation} 已弃用
+     * @deprecated 请使用 {@link #setUserPayload(Object)} 设置自定义用户信息。
+     */
     @Deprecated
     public void setMemberInfo(MemberInformation memberInfo) { this.memberInfo = memberInfo; }
 
