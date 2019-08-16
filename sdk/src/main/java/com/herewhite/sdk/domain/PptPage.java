0983e7c5c7c825940b42df0b292f465994967712
leavesster <11785335+leavesster@users.noreply.github.com>
2019-08-16 12:45:32 +0800

    add javadoc

--- sdk/src/main/java/com/herewhite/sdk/domain/PptPage.java
+++ sdk/src/main/java/com/herewhite/sdk/domain/PptPage.java
@@ -13,6 +13,14 @@ public class PptPage extends WhiteObject {
     private Double width;
     private Double height;
 
+    /**
+     * Ppt，每个场景只有在初始化时，可以配置。src 图片中心点默认为白板内部坐标原点。
+     * 背景图无法拖动，无法改变在白板内部的位置。
+     *
+     * @param src    背景图 url
+     * @param width  在白板中占用的宽度
+     * @param height 在白板中占用的高度
+     */
     public PptPage(String src, Double width, Double height) {
         this.src = src;
         this.width = width;
