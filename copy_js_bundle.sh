c76c9d99761c1a63fb0e8635fbaea3d5f22eb477
leavesster <11785335+leavesster@users.noreply.github.com>
2019-12-20 13:11:57 +0800

    use whiteboard-bridge

--- copy_js_bundle.sh
+++ copy_js_bundle.sh
@@ -3,9 +3,7 @@ BASEDIR=$(cd $(dirname "$0"); pwd -P)
 set -exo pipefail
 cd $BASEDIR
 
-rm -rf sdk/src/main/assets/cloud/
-mkdir sdk/src/main/assets/cloud
-cp -R ../white-native-sdk/build/index.html sdk/src/main/assets/cloud/
-cp -R ../white-native-sdk/build/favicon.ico sdk/src/main/assets/cloud/
-cp -R ../white-native-sdk/build/static sdk/src/main/assets/cloud/
+rm -rf sdk/src/main/assets/
+mkdir sdk/src/main/assets/
+cp -R ../whiteboard-bridge/build/* sdk/src/main/assets/
 rabbit
