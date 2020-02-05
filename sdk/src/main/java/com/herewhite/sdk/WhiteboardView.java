0c7b18088673733c4412f9b5d5eecc8056608253
leavesster <11785335+leavesster@users.noreply.github.com>
2020-02-05 14:23:31 +0800

    try to fix xml error

--- sdk/src/main/java/com/herewhite/sdk/WhiteboardView.java
+++ sdk/src/main/java/com/herewhite/sdk/WhiteboardView.java
@@ -36,6 +36,11 @@ public class WhiteboardView extends DWebView {
         init();
     }
 
+    public WhiteboardView(Context context, AttributeSet attributeSet, int defStyle) {
+        super(context,attributeSet);
+        init();
+    }
+
     private void init() {
         this.getSettings().setMediaPlaybackRequiresUserGesture(false);
         this.loadUrl("file:///android_asset/whiteboard/index.html");
