1d7796e68636f33022b4f42c6ded9b036f0bbef7
leavesster <11785335+leavesster@users.noreply.github.com>
2020-04-21 13:00:45 +0800

    2.7.6

--- sdk/src/main/java/com/herewhite/sdk/WhiteSdk.java
+++ sdk/src/main/java/com/herewhite/sdk/WhiteSdk.java
@@ -29,7 +29,7 @@ public class WhiteSdk {
     private UrlInterrupter urlInterrupter;
 
     public static String Version() {
-        return "2.7.5";
+        return "2.7.6";
     }
 
     public WhiteSdk(WhiteboardView bridge, Context context, WhiteSdkConfiguration whiteSdkConfiguration) {
