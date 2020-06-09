dc6d2935ba0358d21f9549f67aa2a8f7c2d73722
leavesster <11785335+leavesster@users.noreply.github.com>
2020-06-09 13:47:45 +0800

    WhiteSdkConfiguration break change

--- sdk/src/main/java/com/herewhite/sdk/WhiteSdkConfiguration.java
+++ sdk/src/main/java/com/herewhite/sdk/WhiteSdkConfiguration.java
@@ -2,7 +2,6 @@ package com.herewhite.sdk;
 
 import android.os.Build;
 
-import com.google.gson.JsonObject;
 import com.google.gson.annotations.SerializedName;
 import com.herewhite.sdk.domain.DeviceType;
 import com.herewhite.sdk.domain.LoggerOptions;
@@ -23,7 +22,6 @@ public class WhiteSdkConfiguration extends WhiteObject {
         canvas,
     }
 
-    private String appIdentifier;
     public static class PptParams extends WhiteObject {
         public String getScheme() {
             return scheme;
@@ -39,34 +37,37 @@ public class WhiteSdkConfiguration extends WhiteObject {
         }
     }
 
-    private DeviceType deviceType;
-    //TODO: 兼容字段，大版本移除。
-    private double zoomMaxScale;
-    //TODO: 兼容字段，大版本移除。
-    private double zoomMinScale;
-    //会在调用时，直接在 webView 中打印一遍，并回传给 native
-    private boolean debug;
+    // native 永远只接收 touch 事件
+    private DeviceType deviceType = DeviceType.touch;
+    // 在 webView 中，打印日志，并回调给 native 端
+    private boolean log = false;
 
+    private RenderEngineType renderEngine = RenderEngineType.canvas;
+
+    private boolean enableInterrupterAPI = false;
+    private boolean preloadDynamicPPT = false;
+    private boolean routeBackup = false;
+    private boolean userCursor = false;
+    private boolean onlyCallbackRemoteStateModify = false;
+
+    private String appIdentifier;
     private HashMap<String, String> __nativeTags = new HashMap<>();
-
-    public RenderEngineType getRenderEngine() {
-        return renderEngine;
-    }
 
     /**
      * 设置画笔的渲染引擎模式
-     * @param renderEngine 默认为 svg，新增 canvas 模式，对于大量书写做了额外优化
+     * @param renderEngine 画笔教具的渲染模式，对于大量书写做了额外优化。2.9.0 默认切换至 canvas，之前版本为 svg。
      * @since 2.8.0
      */
     public void setRenderEngine(RenderEngineType renderEngine) {
         this.renderEngine = renderEngine;
     }
 
-    private RenderEngineType renderEngine;
-    
-    //TODO: 真实使用字段，大版本对外暴露
-    private boolean enableInterrupterAPI = false;
+    public RenderEngineType getRenderEngine() {
+        return renderEngine;
+    }
 
+    /** pptParams 动态 ppt 专用参数 */
+    private PptParams pptParams;
     public PptParams getPptParams() {
         return pptParams;
     }
@@ -75,14 +76,20 @@ public class WhiteSdkConfiguration extends WhiteObject {
         this.pptParams = pptParams;
     }
 
-    private PptParams pptParams;
 
-    //TODO: 兼容字段，大版本移除。
-    private boolean hasUrlInterrupterAPI = false;
-    private boolean userCursor = false;
-
-    private boolean onlyCallbackRemoteStateModify = false;
     private HashMap<String, String> fonts;
+    public HashMap<String, String> getFonts() {
+        return fonts;
+    }
+
+    /**
+     * 文档转网页（动态 PPT）时，自定义字体，为 key-value 结构
+     * @param fonts key 为字体名称，value 为字体网址的字典结构
+     * @since 2.2.0
+     */
+    public void setFonts(HashMap<String, String> fonts) {
+        this.fonts = fonts;
+    }
 
     public boolean isPreloadDynamicPPT() {
         return preloadDynamicPPT;
@@ -91,7 +98,7 @@ public class WhiteSdkConfiguration extends WhiteObject {
     /**
      * 动态 PPT 预加载选项
      *
-     * 在使用动态 PPT 的同时，加载动态 PPT 中所需要的图片资源
+     * 在使用动态 PPT 的同时，加载动态 PPT 中所需要的图片资源，会导致在第一次加载页面时，出现大量加载。
      *
      * @param preloadDynamicPPT 默认关闭，不进行预加载
      */
@@ -104,22 +111,12 @@ public class WhiteSdkConfiguration extends WhiteObject {
         __nativeTags.put("platform", "android API " + Build.VERSION.SDK_INT);
     }
 
-    private boolean preloadDynamicPPT = false;
-
-    public WhiteSdkConfiguration(String appIdentifier, double zoomMaxScale, double zoomMinScale) {
-        this(appIdentifier, zoomMaxScale, zoomMinScale, false);
-    }
-
-    public WhiteSdkConfiguration(String appIdentifier, double zoomMaxScale, double zoomMinScale, boolean log) {
+    public WhiteSdkConfiguration(String appIdentifier, boolean log) {
         this(appIdentifier);
-        this.zoomMaxScale = zoomMaxScale;
-        this.zoomMinScale = zoomMinScale;
-        this.debug = log;
+        this.log = log;
     }
 
     public WhiteSdkConfiguration(String appIdentifier) {
-        deviceType = DeviceType.touch;
-        renderEngine = RenderEngineType.svg;
         this.appIdentifier = appIdentifier;
         setupNativeTags();
     }
@@ -128,9 +125,8 @@ public class WhiteSdkConfiguration extends WhiteObject {
     public LoggerOptions getLoggerOptions() {
         return loggerOptions;
     }
-
     /**
-     * 日志上报系统设置项
+     * 日志上报系统设置项，有默认上报行为
      *
      * @param loggerOptions {@link LoggerOptions}
      * @since 2.4.2
@@ -152,23 +148,7 @@ public class WhiteSdkConfiguration extends WhiteObject {
         this.routeBackup = routeBackup;
     }
 
-    private boolean routeBackup;
-
     private LoggerOptions loggerOptions;
-
-    public HashMap<String, String> getFonts() {
-        return fonts;
-    }
-
-    /**
-     * 文档转网页（动态 PPT）时，自定义字体地址。key-value 结构
-     *
-     * @param fonts
-     * @since 2.2.0
-     */
-    public void setFonts(HashMap<String, String> fonts) {
-        this.fonts = fonts;
-    }
 
     public DeviceType getDeviceType() {
         return deviceType;
@@ -196,63 +176,29 @@ public class WhiteSdkConfiguration extends WhiteObject {
         this.deviceType = deviceType;
     }
 
-    public double getZoomMaxScale() {
-        return zoomMaxScale;
-    }
-
-    public void setZoomMaxScale(double zoomMaxScale) {
-        this.zoomMaxScale = zoomMaxScale;
-    }
-
-    public double getZoomMinScale() {
-        return zoomMinScale;
-    }
-
-    public void setZoomMinScale(double zoomMinScale) {
-        this.zoomMinScale = zoomMinScale;
-    }
-
-    public boolean isDebug() {
-        return debug;
+    public boolean isLog() {
+        return log;
     }
 
     /**
      * 打印 debug 日志
      *
-     * @param debug 默认关闭
+     * @param log 默认关闭
      */
-    public void setDebug(boolean debug) {
-        this.debug = debug;
+    public void setLog(boolean log) {
+        this.log = log;
     }
 
-    public boolean isHasUrlInterrupterAPI() {
-        return hasUrlInterrupterAPI;
+    public boolean isEnableInterrupterAPI() {
+        return enableInterrupterAPI;
     }
 
     /**
      * 设置图片替换 API
      *
-     * @param hasUrlInterrupterAPI 图片替换开关，默认关闭
+     * @param enableInterrupterAPI 图片替换开关，默认关闭
      */
-    public void setHasUrlInterrupterAPI(boolean hasUrlInterrupterAPI) {
-        this.enableInterrupterAPI = hasUrlInterrupterAPI;
-        this.hasUrlInterrupterAPI = hasUrlInterrupterAPI;
-    }
-
-    @SerializedName("initializeOriginsStates")
-    private JsonObject sdkStrategyConfig;
-
-    /**
-     * 传入服务器连接信息
-     * @deprecated 从 2.8.0 开始，sdk 采用更智能的链路选择，不再需要主动传入该参数
-     * @param jsonObject
-     * @since 2.5.4
-     */
-    public void setSdkStrategyConfig(JsonObject jsonObject) {
-        this.sdkStrategyConfig = jsonObject;
-    }
-
-    public JsonObject getSdkStrategyConfig() {
-        return sdkStrategyConfig;
+    public void setEnableInterrupterAPI(boolean enableInterrupterAPI) {
+        this.enableInterrupterAPI = enableInterrupterAPI;
     }
 }
