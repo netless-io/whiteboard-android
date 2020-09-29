eab688e1351b3bf2dcd83b15398f42620be0fc72
leavesster <11785335+leavesster@users.noreply.github.com>
2020-09-29 16:07:09 +0800

    iframe-dev

--- app/src/main/java/com/herewhite/demo/DemoAPI.java
+++ app/src/main/java/com/herewhite/demo/DemoAPI.java
@@ -32,7 +32,7 @@ public class DemoAPI {
 
     static final MediaType JSON
             = MediaType.parse("application/json; charset=utf-8");
-    private static final String sdkToken = "WHITEcGFydG5lcl9pZD1OZ3pwQWNBdlhiemJERW9NY0E0Z0V3RTUwbVZxM0NIbDJYV0Ymc2lnPWNiZWExOTMwNzc1NmQyNmU3N2U3M2Q0NWZjNTZiOGIwMWE2ZjU4NDI6YWRtaW5JZD0yMTYmcm9sZT1hZG1pbiZleHBpcmVfdGltZT0xNTg5ODMzNTQxJmFrPU5nenBBY0F2WGJ6YkRFb01jQTRnRXdFNTBtVnEzQ0hsMlhXRiZjcmVhdGVfdGltZT0xNTU4Mjc2NTg5Jm5vbmNlPTE1NTgyNzY1ODg4NDQwMA";
+    private static final String sdkToken = "NETLESSSDK_YWs9M2R5WmdQcFlLcFlTdlQ1ZjRkOFBiNjNnY1RoZ3BDSDlwQXk3Jm5vbmNlPTE2MDExOTk4NjQ5MzIwMCZyb2xlPTAmc2lnPTQxYmExOWZiYjIzOTUyY2M1ZDhmMDdiNmU3ODEzNTY4OGQ0YTQzYTg4NWI4M2ExNTA3N2NjZGQxYjE3MDQ4N2I";
     private static final String host = "https://cloudcapiv4.herewhite.com";
 
     public String getAppIdentifier() {
@@ -40,8 +40,8 @@ public class DemoAPI {
     }
     private String TAG = "demo api";
     private String AppIdentifier = "792/uaYcRG0I7ctP9A";
-    private String demoUUID = "daef60b584ea4892a381c410ae15fe28";
-    private String demoRoomToken = "WHITEcGFydG5lcl9pZD1ZSEpVMmoxVXAyUzdoQTluV3dvaVlSRVZ3MlI5M21ibmV6OXcmc2lnPWJkODdlOGFkZDcwZmEzN2YzNWQ3OTAyYmViMWFlMDk2YjQ1ZWI0MmM6YWRtaW5JZD02Njcmcm9vbUlkPWRhZWY2MGI1ODRlYTQ4OTJhMzgxYzQxMGFlMTVmZTI4JnRlYW1JZD03OTImcm9sZT1yb29tJmV4cGlyZV90aW1lPTE2MTIwMzU1MTgmYWs9WUhKVTJqMVVwMlM3aEE5bld3b2lZUkVWdzJSOTNtYm5lejl3JmNyZWF0ZV90aW1lPTE1ODA0Nzg1NjYmbm9uY2U9MTU4MDQ3ODU2NTczODAw";
+    private String demoUUID = "";
+    private String demoRoomToken = "";
 
 
     String getDemoUUID() {
