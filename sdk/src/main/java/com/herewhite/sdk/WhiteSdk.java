66a7bc1d707ba932ac05f9b1ea762dff7c584b33
leavesster <11785335+leavesster@users.noreply.github.com>
2020-12-04 13:59:02 +0800

    2.11.3

--- sdk/src/main/java/com/herewhite/sdk/WhiteSdk.java
+++ sdk/src/main/java/com/herewhite/sdk/WhiteSdk.java
@@ -55,7 +55,7 @@ public class WhiteSdk {
     private AudioMixerImplement audioMixerImplement;
 
     public static String Version() {
-        return "2.11.2";
+        return "2.11.3";
     }
 
     public WhiteSdk(WhiteboardView bridge, Context context, WhiteSdkConfiguration whiteSdkConfiguration, @Nullable CommonCallbacks commonCallbacks) {
