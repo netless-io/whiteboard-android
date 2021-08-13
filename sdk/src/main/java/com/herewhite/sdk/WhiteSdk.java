b07f3ceeeb7b24b9846a23d99ac75d2046ba2bb5
fenglibin <fenglibin@agora.io>
2021-08-13 09:58:29 +0800

    update web sdk

--- sdk/src/main/java/com/herewhite/sdk/WhiteSdk.java
+++ sdk/src/main/java/com/herewhite/sdk/WhiteSdk.java
@@ -25,7 +25,7 @@ import wendu.dsbridge.OnReturnValue;
  * `WhiteSdk` 类。
  */
 public class WhiteSdk {
-    private final static String SDK_VERSION = "2.13.21";
+    private final static String SDK_VERSION = "2.13.22";
 
     private final static Gson gson = new Gson();
 
