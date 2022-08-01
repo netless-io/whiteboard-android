7d3a1e60b5f400a2d36537796dcf831d4a80fe4f
aderan <fenglibinjie@gmail.com>
2022-08-01 10:55:03 +0800

    release 2.16.27

--- sdk/src/main/java/com/herewhite/sdk/WhiteSdk.java
+++ sdk/src/main/java/com/herewhite/sdk/WhiteSdk.java
@@ -29,7 +29,7 @@ import wendu.dsbridge.OnReturnValue;
  * `WhiteSdk` 类。
  */
 public class WhiteSdk {
-    private final static String SDK_VERSION = "2.16.26";
+    private final static String SDK_VERSION = "2.16.27";
 
     private final static Gson gson = new Gson();
 
