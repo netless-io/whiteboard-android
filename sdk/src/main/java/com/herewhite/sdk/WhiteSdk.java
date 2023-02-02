bd985fc23d4d5685b6709f203aaff164a9cb316f
aderan <fenglibinjie@gmail.com>
2023-02-02 17:43:38 +0800

    release 2.16.49

--- sdk/src/main/java/com/herewhite/sdk/WhiteSdk.java
+++ sdk/src/main/java/com/herewhite/sdk/WhiteSdk.java
@@ -35,7 +35,7 @@ import wendu.dsbridge.OnReturnValue;
  * `WhiteSdk` 类。
  */
 public class WhiteSdk {
-    private final static String SDK_VERSION = "2.16.48";
+    private final static String SDK_VERSION = "2.16.49";
 
     private final static Gson gson = new Gson();
     private static AudioMixerBridge sAudioMixerBridge;
