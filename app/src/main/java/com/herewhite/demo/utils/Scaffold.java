a2de47cd8592e6ac9f507681f5c458abd537b7b3
aderan <fenglibinjie@gmail.com>
2023-04-11 14:49:06 +0800

    add Scaffold code

---
+++ app/src/main/java/com/herewhite/demo/utils/Scaffold.java
@@ -0,0 +1,53 @@
+package com.herewhite.demo.utils;
+
+public class Scaffold {
+    /**
+     * save and restore camera state
+     *
+     * whiteboardView.addOnLayoutChangeListener { _, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
+     *     if (left == oldLeft && top == oldTop && right == oldRight && bottom == oldBottom) {
+     *         return@addOnLayoutChangeListener
+     *     }
+     *
+     *     if (right - left < LimitedWidth) {
+     *         cameraState = room.roomState.cameraState
+     *     } else {
+     *         cameraState?.let {
+     *             val config = CameraConfig().apply {
+     *                 scale = it.scale
+     *                 centerX = it.centerX
+     *                 centerY = it.centerY
+     *             }
+     *             room.moveCamera(config)
+     *         }
+     *     }
+     * }
+     */
+
+    /**
+     * hide and show title bar
+     *
+     * String cssHide = "" +
+     *         "var elements = document.querySelectorAll(\".telebox-titlebar, .telebox-titlebar-wrap\");\n" +
+     *         "for (var i = 0; i < elements.length; i++) {\n" +
+     *         "    elements[i].style.display = \"none\";\n" +
+     *         "} \n";
+     *
+     * String cssShow = "" +
+     *         "var elements = document.querySelectorAll(\".telebox-titlebar, .telebox-titlebar-wrap\");\n" +
+     *         "for (var i = 0; i < elements.length; i++) {\n" +
+     *         "    elements[i].style.display = \"\";\n" +
+     *         "} \n";
+     *
+     * mWhiteboardView.evaluateJavascript(cssShow, null);
+     *
+     * String hideTitleBar = "" +
+     *         "if (!window.__hideTitleBar) {\n" +
+     *         "  var style = document.createElement('style')\n" +
+     *         "  style.innerText = '.telebox-titlebar, .telebox-titlebar-wrap { display: none; }'\n" +
+     *         "  window.__hideTitleBar = style\n" +
+     *         "}\n" +
+     *         "document.head.appendChild(window.__hideTitleBar)";
+     * mWhiteboardView.evaluateJavascript(hideTitleBar, null);
+     */
+}
