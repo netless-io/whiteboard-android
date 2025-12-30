3ecb364a1e9dbf948cfc3a193594d485da50dd99
aderan <fenglibinjie@gmail.com>
2025-12-30 15:31:17 +0800

    update demo

--- app/src/main/java/com/herewhite/demo/MainApplication.java
+++ app/src/main/java/com/herewhite/demo/MainApplication.java
@@ -13,5 +13,6 @@ public class MainApplication extends Application {
         DemoAPI.get().init(getApplicationContext());
         WhiteboardView.setWebContentsDebuggingEnabled(true);
         // WhiteSdk.prepareWhiteConnection(this, new ConnectionPrepareParam("123/12312", Region.cn));
+        // WhiteboardView.enableAssetsHttps();
     }
 }
